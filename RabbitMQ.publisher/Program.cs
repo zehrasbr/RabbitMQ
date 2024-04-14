29bfddbe2e82dc411c1b88c42e5c93c8c6c04c39
zehrasbr <zehraisbr@gmail.com>
2024-04-14 21:50:42 +0300

    update

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -15,17 +15,16 @@ namespace RabbitMQ.publisher
 
             //bu kanal üzerinden rabbitmq ile haberleşebiliriz.
             var channel = connection.CreateModel();
-
             //kuyruğu burada oluştururuz.
             //true yaparsak rabbitmq'ya restart atsakta kuyruklar kaybolmaz.
             //subscriber'a bağlanmak için(farklı kanallardan bağlanmak için) false yaptık.
             //Eğer sadece bulunduğu kanala bağlanmak istesek true olmalı.  
             //autoDelete otomatik silinmesini istemediğimiz için false.
-            channel.QueueDeclare("hello-queue", true, false,false);
+            channel.QueueDeclare("hello-queue", true, false, false);
 
             string message = "hello world";
             var messageBody = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublish(string.Empty, "hello-queue",null,messageBody);
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
             Console.WriteLine("Mesaj gönderildi.");
             Console.ReadLine();
         }
