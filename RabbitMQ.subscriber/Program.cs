413bd4397146e35549c63186290298d6d165bb85
z <z@DESKTOP-K1N62CJ>
2024-04-14 21:41:32 +0300

    update

--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Text;
 
@@ -17,9 +18,20 @@ namespace RabbitMQ.subscriber
 
             //kuyruk burada oluşturmak zorunda değiliz.
             //eğer oluşturacaksak publisher kısmı ile aynı olmalı kuyruk.
-            channel.QueueDeclare("hello-queue", true, false, false);
+            //channel.QueueDeclare("hello-queue", true, false, false);
 
-            
+            var consumer = new EventingBasicConsumer(channel);
+
+            //autoAck false verirsek RabbitMQ subscriber'a mesaj gönderdiğinde bu mesaj doğru veya yanlış işlensede bu kuyruktan siler.
+            //autoAck true verirsek gelen mesajı doğru bir şekilde işlerse kuyruktan silinmesi için haber eder.
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj:" + message);
+            };
+                
             Console.ReadLine();
         }
     }
